a33f2c7caeebe28a467ae2fe1c05234bfe10d462
Bruno <bruno@awdd.com.br>
2025-03-24 21:36:02 -0300

    fix: start db

--- cube_client/lib/main.dart
+++ cube_client/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'pages/upload_page.dart';
+import 'services/db_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await DbService.init();
   runApp(const PhotoUploaderApp());
 }
 
