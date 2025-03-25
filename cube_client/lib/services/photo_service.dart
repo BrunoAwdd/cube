be589421e01a1a3e967a252946bc8d456beb4d83
Bruno <bruno@awdd.com.br>
2025-03-25 18:56:02 -0300

    fix: limit in loadUnsentPhotos

--- cube_client/lib/services/photo_service.dart
+++ cube_client/lib/services/photo_service.dart
@@ -4,9 +4,9 @@ import 'package:crypto/crypto.dart';
 import 'db_service.dart';
 
 class PhotoService {
-  static Future<List<AssetEntity>> loadUnsentPhotos() async {    final albums = await PhotoManager.getAssetPathList(type: RequestType.image);
+  static Future<List<AssetEntity>> loadUnsentPhotos({limit: 5}) async {    final albums = await PhotoManager.getAssetPathList(type: RequestType.image);
     final recent = albums.first;
-    final assets = await recent.getAssetListPaged(page: 0, size: 5);
+    final assets = await recent.getAssetListPaged(page: 0, size: limit);
 
     for (final asset in assets) {
       final file = await asset.originFile;
