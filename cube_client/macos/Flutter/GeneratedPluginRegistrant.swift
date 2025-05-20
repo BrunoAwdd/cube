c059dd14e596784fb2319ac307b75061d74e6bfa
Bruno <bruno@awdd.com.br>
2025-05-20 15:03:58 -0300

    flutter files

--- cube_client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ cube_client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import mobile_scanner
 import path_provider_foundation
 import photo_manager
 import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
